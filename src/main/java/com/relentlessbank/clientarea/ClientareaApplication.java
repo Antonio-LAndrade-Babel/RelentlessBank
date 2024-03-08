250358a531b744f3bd43c0f9bbd6e8247919c9b6
Antonio-LAndrade-Babel <antonio.landrade@babelgroup.com>
2024-03-08 11:15:02 +0100

    Commit inicial
    Aquí comienza nuestra aventura.

---
+++ src/main/java/com/relentlessbank/clientarea/ClientareaApplication.java
@@ -0,0 +1,13 @@
+package com.relentlessbank.clientarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientareaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relentlessbank/clientarea/ClientareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relentlessbank.clientarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
