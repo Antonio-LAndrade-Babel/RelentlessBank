b3684daa81de553f7bd08094c1d2721456b312d1
Antonio-LAndrade-Babel <antonio.landrade@babelgroup.com>
2024-03-09 20:12:05 +0100

    Implementados los métodos de BankAccountController
    Implementados los métodos de BankAccountController

--- src/main/java/com/relentlessbank/clientarea/controller/BankAccountController.java
+++ src/main/java/com/relentlessbank/clientarea/controller/BankAccountController.java
@@ -7,34 +7,39 @@ import com.relentlessbank.clientarea.service.ClientService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/bankaccount")
 public class BankAccountController {
     private final BankAccountService bankAccountService;
-    private final ClientService clientService;
-    public BankAccountController(BankAccountService bankAccountService, ClientService clientService) {
+    public BankAccountController(BankAccountService bankAccountService) {
         this.bankAccountService = bankAccountService;
-        this.clientService = clientService;
     }
-    @PostMapping("/create")
-    public ResponseEntity<BankAccount> createBankAccount(){
-        //TODO
-        return null;
+    @PostMapping("/create/{clientId}")
+    public ResponseEntity<BankAccount> createBankAccount(@PathVariable int clientId, @RequestBody BankAccount bankAccount) {
+        BankAccount newBankAccount = bankAccountService.createBankAccount(clientId, bankAccount);
+        return ResponseEntity.ok(newBankAccount);
     }
     @PostMapping("/update/{id}")
-    public ResponseEntity<BankAccount> updateBankAccount(){
-        //TODO
-        return null;
+    public ResponseEntity<BankAccount> updateBankAccount(@PathVariable int id, @RequestBody BankAccount bankAccountDetails) {
+        BankAccount bankAccount = bankAccountService.updateBankAccount(id, bankAccountDetails);
+        return ResponseEntity.ok(bankAccount);
     }
-    @PostMapping("/find")
-    public ResponseEntity<BankAccount> getBankAccountById(){
-        //TODO
-        return null;
+    @PostMapping("/find/{id}")
+    public ResponseEntity<BankAccount> getBankAccountById(@PathVariable int id){
+        BankAccount bankAccount = bankAccountService.getBankAccountById(id);
+        return ResponseEntity.ok(bankAccount);
     }
-    @PostMapping("/delete")
-    public ResponseEntity<BankAccount> deleteBankAccount(){
-        //TODO
-        return null;
+    @PostMapping("/findallbyclient/{clientId}")
+    public ResponseEntity<List<BankAccount>> getAllBankAccountByClient(@PathVariable int clientId) {
+        List<BankAccount> bankAccounts = bankAccountService.getAllBankAccountByClient(clientId);
+        return ResponseEntity.ok(bankAccounts);
+    }
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<BankAccount> deleteBankAccount(@PathVariable int id) {
+        bankAccountService.deleteBankAccount(id);
+        return ResponseEntity.ok().build();
     }
 
 }
