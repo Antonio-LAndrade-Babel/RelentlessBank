d8b310959add6094437960c0be54ea7818bd70da
Antonio-LAndrade-Babel <antonio.landrade@babelgroup.com>
2024-03-09 17:45:46 +0100

    Creada la interfaz BankAccountService.
    Creada la interfaz BankAccountService.

---
+++ src/main/java/com/relentlessbank/clientarea/service/BankAccountService.java
@@ -0,0 +1,15 @@
+package com.relentlessbank.clientarea.service;
+
+import com.relentlessbank.clientarea.model.BankAccount;
+import com.relentlessbank.clientarea.model.Client;
+
+import java.util.List;
+
+public interface BankAccountService {
+    BankAccount createBankAccount (int clientId, BankAccount bankAccount);
+    BankAccount updateBankAccount (int id, BankAccount bankAccount);
+    List<BankAccount> getAllBankAccounts();
+    BankAccount getBankAccountById(int id);
+    List<BankAccount> getAllBankAccountByClient(Client client);
+    void deleteBankAccount(int id);
+}
